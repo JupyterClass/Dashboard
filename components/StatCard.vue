fe998d6bec86d33583e0a6807739a12c3bc2e671
elihuansen <elihuansen@gmail.com>
2020-06-03 05:11:09 +0800

    Added Statistics component

---
+++ components/StatCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="statcard">
+    <a-icon class="icon" :type="icon" theme="twoTone" :twoToneColor="accentColor" />
+    <div class="main-content">
+      <h1 class="title">{{ title }}</h1>
+      <h1 class="stat">{{ stat }}</h1>
+    </div>
+    <div class="statcard-accent" :style="{backgroundColor: accentColor}"/>
+  </div>
+</template>
+
+<script>
+import { Icon as AIcon } from 'ant-design-vue';
+export default {
+  name: "StatCard",
+  components: { AIcon },
+  props: ['accentColor', 'title', 'stat', 'icon']
+};
+</script>
+
+<style scoped>
+.statcard {
+  display: flex;
+  align-items: center;
+  position: relative;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px 1px #d0ddf0;
+  overflow: hidden;
+  padding: 8px 24px 0 16px;
+  min-width: 250px;
+}
+
+.icon {
+  font-size: 3.5em;
+  color: #486177;
+  margin-right: 16px;
+}
+
+.main-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  text-align: right;
+}
+
+.title {
+  font-size: 1.5em;
+  letter-spacing: 4px;
+  text-transform: uppercase;
+  margin: 0;
+}
+
+.stat {
+  font-size: 2.5em;
+  margin: 0;
+}
+
+.statcard-accent {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 8px;
+  height: 100%;
+}
+</style>
---
+++ components/Statistics.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="statistics">
+    <stat-card title="completed"
+               stat="88%"
+               icon="pie-chart"
+               accent-color="#3eb5ff"/>
+    <stat-card title="Avg speed"
+               stat="10m 43s"
+               icon="dashboard"
+               accent-color="#3eb5ff"/>
+  </div>
+</template>
+
+<script>
+import StatCard from "./StatCard";
+export default {
+  name: "Statistics",
+  components: { StatCard }
+};
+</script>
+
+<style scoped>
+.statistics {
+  display: flex;
+}
+.statistics > div {
+  margin: 10px;
+  color: #3eb5ff;
+}
+</style>

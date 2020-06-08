d738373605f4dd64502df136d2aa5f3118bc1313
elihuansen <elihuansen@gmail.com>
2020-06-08 17:11:05 +0800

    Woops

---
+++ components/Accent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="accent"
+       :style="`${alignment}: 0; background-color: ${color}`"/>
+</template>
+
+<script>
+export default {
+  name: "Accent",
+  props: ['color', 'alignment'],
+};
+</script>
+
+<style scoped>
+.accent {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 8px;
+  height: 100%;
+  transition: background-color 0.3s;
+}
+</style>
---
+++ components/Clock.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-bind="$attrs">
+    {{ currentTime }}
+  </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+
+export default {
+  name: "Clock",
+  data() {
+    return {
+      intervalFn: null,
+      currentTime: dayjs(Date.now()).format('hh:mm:ss a'),
+    }
+  },
+  mounted() {
+    this.intervalFn = setInterval(() => {
+      this.currentTime = dayjs(Date.now()).format('hh:mm:ss a');
+    }, 1000);
+  }
+};
+</script>
+
+<style scoped>
+
+</style>

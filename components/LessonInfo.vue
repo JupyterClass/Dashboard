4b9538125eaf5afe348198a3d35b3ab04f2f16a3
elihuansen <elihuansen@gmail.com>
2020-06-12 20:42:19 +0800

    Added confirmation prompt for delete notebook button

--- components/LessonInfo.vue
+++ components/LessonInfo.vue
@@ -2,7 +2,13 @@
   <div class="lesson-info">
     <div v-if="selectedNotebook">
       <h1>{{selectedNotebook.id }}</h1>
-      <a-button ghost type="danger" icon="delete" shape="circle" @click="handleDeleteClick" />
+      <a-popconfirm placement="bottom" @confirm="handleDeleteClick">
+        <template slot="title">
+          <p>Confirm delete notebook?</p>
+          <p>This will erase all related to this notebook!</p>
+        </template>
+        <a-button ghost type="danger" icon="delete" shape="circle" />
+      </a-popconfirm>
       <a-button ghost type="primary" icon="question" shape="circle" />
     </div>
     <clock class="clock"/>
@@ -11,11 +17,14 @@
 
 <script>
 import socket from '~/plugins/socket.io.js';
-import { Button as AButton } from "ant-design-vue";
+import {
+  Button as AButton,
+  Popconfirm as APopconfirm,
+} from "ant-design-vue";
 import Clock from "./Clock";
 export default {
   name: "LessonInfo",
-  components: { AButton, Clock },
+  components: { AButton, APopconfirm, Clock },
   methods: {
     handleDeleteClick() {
       socket.emit('client-delete-practice', this.$store.state.selectedNotebook.id);
